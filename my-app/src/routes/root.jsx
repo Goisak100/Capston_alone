c5603d0a9bc67423938e71cfea89cf32480894b1
goisak100 <goisak100@naver.com>
2023-06-30 10:19:37 +0900

    Set react-router-dom Capston path

---
+++ my-app/src/routes/root.jsx
@@ -0,0 +1,14 @@
+import { useEffect } from "react"
+
+export default function Root() {
+
+    useEffect(() => {
+        
+    }, [])
+
+    return (
+        <div>
+            <h1>Root 페이지입니다.</h1>
+        </div>
+    )
+}
