0db72136aba858c5c419ebdd7776bbdbec68d3c5
goisak100 <goisak100@naver.com>
2023-07-05 22:42:11 +0900

    로딩스피너 className에 오탈자 수정;

--- my-app/src/components/loadingSpinner.jsx
+++ my-app/src/components/loadingSpinner.jsx
@@ -3,7 +3,7 @@ import "./components.css"
 export default function LoadingSpinner() {
     return (
         <div className="__loading-container">
-            <div className="__loading-spinner="></div>
+            <div className="__loading-spinner"></div>
         </div>
     )
 }
