06406f5d6873d627d86653b4c3b8e1755e8bccb5
goisak100 <goisak100@naver.com>
2023-07-05 22:35:23 +0900

    [간단 수정] 컴포넌트로 추출한 로딩스피너의 css 이름을 유니크하게 변경함

--- my-app/src/components/loadingSpinner.jsx
+++ my-app/src/components/loadingSpinner.jsx
@@ -2,8 +2,8 @@ import "./components.css"
 
 export default function LoadingSpinner() {
     return (
-        <div className="loading-spinner-component-container">
-            <div className="loading-spinner-component"></div>
+        <div className="__loading-container">
+            <div className="__loading-spinner="></div>
         </div>
     )
 }
--- my-app/src/imageUpload.jsx
+++ my-app/src/imageUpload.jsx
@@ -159,7 +159,7 @@ export default function ImageUpload({ onClose }) {
                             </div>
                         </div>
                     }
-                    
+
                     {currentPage === 2 &&
                         <div>
                             {isLoading &&
